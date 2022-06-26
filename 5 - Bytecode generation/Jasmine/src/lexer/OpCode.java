33e4dc5313582bbef58caea1089d4107bcdc4652
Federico Filì <32766721+zfeder@users.noreply.github.com>
2022-06-26 11:51:16 +0200

    Delete 5 - Bytecode generation/Jasmine directory

--- 5 - Bytecode generation/Jasmine/src/lexer/OpCode.java
+++
@@ -1,7 +0,0 @@
-package lexer;
-
-public enum OpCode {
-    ldc, imul, ineg, idiv, iadd,
-    isub, istore, ior, iand, iload,
-    if_icmpeq, if_icmple, if_icmplt, if_icmpne, if_icmpge,
-    if_icmpgt, ifne, GOto, invokestatic, label, dup }
